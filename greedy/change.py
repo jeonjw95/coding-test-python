220762409e3e4c14208aa037683622453472fcfd
전진우 <jeonjin-u@jeonjin-uui-MacBookPro.local>
2022-05-13 15:27:54 +0900

    change problem solved

---
+++ greedy/change.py
@@ -0,0 +1,11 @@
+def change_coin_count(change):
+    coins = (500, 100, 50, 10)
+    n = 0
+    for coin in coins:
+        n += change // coin
+        change %= coin
+
+    return n
+
+
+print(change_coin_count(1260))
